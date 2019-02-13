26c4a1a7b3a6a750c9856272b4bfa9e5c49deedf
1dam <1dam@hz319576.TartangaLH.eus>
2019-02-13 10:38:15 +0100

    Commit test

---
+++ Refactorización/src/FicheroAleatorioVentana.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import javax.swing.JFrame;
+
+public class FicheroAleatorioVentana {
+
+	public static void main(String[] args) throws IOException {
+		
+		JFrame f = new JFrame("DEPARTAMENTOS.");
+		// Nos aseguramos que el fichero exista para evitar posibles errores
+		File fichero = new File("AleatorioDep.dat");
+		RandomAccessFile file;
+		file = new RandomAccessFile(fichero, "rw");	
+		file.close();
+		
+		VentanaDepart v = new VentanaDepart(f);
+		v.setVisible(true);
+	   
+	}//fin main
+	
+	class clasaeAnidada {
+		void entrada() {
+			System.out.println("Método entrada.");
+		}
+		
+		String salida (int d) {
+			System.out.println("Salida.");
+			return "Salida el " + d;
+		}
+	}
+
+	
+}//fin class
